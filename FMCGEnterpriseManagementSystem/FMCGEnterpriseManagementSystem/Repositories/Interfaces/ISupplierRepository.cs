5931fe043e659e252de4e360ca56e484015977f4
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-02 11:47:20 +0200

    add ISupplierRepository and SupplierRepository

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Repositories/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using FMCGEnterpriseManagementSystem.Models;
+
+namespace FMCGEnterpriseManagementSystem.Repositories.Interfaces
+{
+    public interface ISupplierRepository
+    {
+        Task<IEnumerable<Supplier>> GetAllAsync();
+        Task<Supplier?> GetByIdAsync(string id);
+        Task AddAsync(Supplier supplier);
+        Task UpdateAsync(Supplier supplier);
+        Task DeleteAsync(string id);
+    }
+}
