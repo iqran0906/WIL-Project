f2b915ff4627bf1ed5b2857bb4ebee243254daaa
iqran0906 <165156434+iqran0906@users.noreply.github.com>
2026-09-08 08:24:33 +0200

    User Management Model Created

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/ViewModels/CreateUserAccountViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FMCGEnterpriseManagementSystem.ViewModels
+{
+    public class CreateUserAccountViewModel
+    {
+        [Required]
+        public string EmployeeID { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Temporary Password")]
+        public string TemporaryPassword { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(
+            nameof(TemporaryPassword),
+            ErrorMessage = "The passwords do not match.")]
+        [Display(Name = "Confirm Temporary Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
