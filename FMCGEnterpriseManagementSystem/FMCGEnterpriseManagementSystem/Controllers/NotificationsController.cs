da26f3a990503dbe8c2c97a0dd19f2984e7e9c78
Sayali-St10458649 <st10458649@myemeris.edu.za>
2026-09-16 08:23:31 +0200

    Create NotificationsController.cs

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Controllers/NotificationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FMCGEnterpriseManagementSystem.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
