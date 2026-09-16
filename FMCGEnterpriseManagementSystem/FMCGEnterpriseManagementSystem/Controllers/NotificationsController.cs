32b7421e52de37e957de0516ab3c70c8ed16f1a7
Sayali-St10458649 <st10458649@myemeris.edu.za>
2026-09-16 08:47:08 +0200

    Created/Fixed the views
    Edited HomeController and NotifactionsController
    
    Still need to run a migration for Notifcations so i am able to test it

--- FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Controllers/NotificationsController.cs
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Controllers/NotificationsController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FMCGEnterpriseManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace FMCGEnterpriseManagementSystem.Controllers
 {
     public class NotificationsController : Controller
     {
-        public IActionResult Index()
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
         {
-            return View();
+            _notificationService = notificationService;
+        }
+
+        // GET: Notifications
+        public async Task<IActionResult> Index()
+        {
+            var notifications = await _notificationService.GetAllAsync();
+            return View(notifications);
+        }
+
+        // GET: Notifications/Unread (for a bell icon dropdown, or a filtered view)
+        public async Task<IActionResult> Unread()
+        {
+            var notifications = await _notificationService.GetUnreadAsync();
+            return View("Index", notifications);
+        }
+
+        // POST: Notifications/MarkAsRead/5
+        [HttpPost]
+        public async Task<IActionResult> MarkAsRead(int id)
+        {
+            await _notificationService.MarkAsReadAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
