bd64efd80d79469a4230255ea768040f68091012
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-16 13:14:01 +0200

    Create IInventoryRepository.cs

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Repositories/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using FMCGEnterpriseManagementSystem.Models;
+
+namespace FMCGEnterpriseManagementSystem.Repositories.Interfaces
+{
+    public interface IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> GetAllAsync();
+        Task<Inventory?> GetByIdAsync(int id);
+        Task<Inventory?> GetByProductIdAsync(int productId);
+        Task AddAsync(Inventory inventory);
+        Task UpdateAsync(Inventory inventory);
+        Task DeleteAsync(int id);
+    }
+}
