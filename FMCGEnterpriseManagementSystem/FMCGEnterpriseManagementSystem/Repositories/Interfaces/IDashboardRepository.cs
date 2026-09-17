d94fbdb771f746937502bbe76da1726175c0fe1d
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-17 08:41:46 +0200

    Add IDashboardRepository

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Repositories/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,14 @@
+﻿using FMCGEnterpriseManagementSystem.Models;
+
+namespace FMCGEnterpriseManagementSystem.Repositories.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<int> GetTotalProductsCountAsync();
+        Task<int> GetTotalSuppliersCountAsync();
+        Task<int> GetTotalCustomersCountAsync();
+        Task<decimal> GetTotalInventoryValueAsync();
+        Task<IEnumerable<Inventory>> GetLowStockInventoriesAsync();
+        Task<Dictionary<string, (int ItemCount, int TotalQty)>> GetCategoryStockSummariesAsync();
+    }
+}
