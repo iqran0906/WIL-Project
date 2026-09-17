ddd190ef57a937ed2fc9dbe409896184cb4d733d
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-17 08:32:11 +0200

    Add dashboard analytics service interface

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Services/Interfaces/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using FMCGEnterpriseManagementSystem.ViewModels;
+
+namespace FMCGEnterpriseManagementSystem.Services.Interfaces
+{
+    public interface IDashboardService
+    {
+        Task<DashboardViewModel> GetDashboardAnalyticsAsync();
+    }
+}
