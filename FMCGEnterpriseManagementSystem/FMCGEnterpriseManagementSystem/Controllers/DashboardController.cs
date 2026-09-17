a4e64278be21b4fa208b4e47af91c204649bfabc
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-17 08:35:35 +0200

    Add dashboard controller

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using FMCGEnterpriseManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMCGEnterpriseManagementSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        // GET: /Dashboard
+        public async Task<IActionResult> Index()
+        {
+            var analytics = await _dashboardService.GetDashboardAnalyticsAsync();
+            return View("~/Views/Dashboard/Index.cshtml", analytics);
+        }
+    }
+}
