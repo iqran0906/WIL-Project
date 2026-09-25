1f6b7e1f9f20293a3516ffda389b978084a7c46c
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-25 17:52:58 +0200

    Add Analytics controller and view model

---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Controllers/AnalyticsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FMCGEnterpriseManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMCGEnterpriseManagementSystem.Controllers
+{
+    [Authorize]
+    public class AnalyticsController : Controller
+    {
+        private readonly IAnalyticsService _analyticsService;
+
+        public AnalyticsController(IAnalyticsService analyticsService)
+        {
+            _analyticsService = analyticsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _analyticsService.GetSalesAndInventoryTrendsAsync();
+            return View(model);
+        }
+    }
+}
---
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/ViewModels/AnalyticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FMCGEnterpriseManagementSystem.ViewModels
+{
+    public class AnalyticsViewModel
+    {
+        public string[] TrendMonths { get; set; } = Array.Empty<string>();
+        public decimal[] MonthlySalesTotals { get; set; } = Array.Empty<decimal>();
+        public int[] InventoryTurnoverRates { get; set; } = Array.Empty<int>();
+    }
+}
