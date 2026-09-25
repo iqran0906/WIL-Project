3405b18d878aefb47cda2c78fa4025eb0b1df58a
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-25 16:25:15 +0200

    Updated repository

--- FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Repositories/ProductRepository.cs
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Repositories/ProductRepository.cs
@@ -3,11 +3,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using FMCGEnterpriseManagementSystem.Data;
 using FMCGEnterpriseManagementSystem.Models;
-using FMCGEnterpriseManagementSystem.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace FMCGEnterpriseManagementSystem.Repositories
 {
+    // Interface declared directly above the implementation
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetLowStockProductsAsync();
+        Task AddProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+
     public class ProductRepository : IProductRepository
     {
         private readonly ApplicationDbContext _context;
