060baf2f055de8f9fe1d242c0ffe608b6c707d96
Maseeha17 <st10454477@myemeris.edu.za>
2026-09-26 15:43:27 +0200

    Update inventory

--- FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Models/Inventory.cs
+++ FMCGEnterpriseManagementSystem/FMCGEnterpriseManagementSystem/Models/Inventory.cs
@@ -1,30 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FMCGEnterpriseManagementSystem.Models;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema; 
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace FMCGEnterpriseManagementSystem.Models
+public class Inventory
 {
-    public class Inventory
+    [Key]
+    public int InventoryId { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+
+    [ForeignKey("ProductId")]
+    public Product Product { get; set; } = null!;
+
+    public int QuantityOnHand { get; set; }
+
+  
+    public int QuantityInStock
     {
-        [Key]
-        public int InventoryId { get; set; }
-
-        [Required]
-        public int ProductId { get; set; }
-
-        // Explicitly map this navigation property to prevent EF Core from creating 'ProductId1'
-        [ForeignKey("ProductId")]
-        public Product Product { get; set; } = null!;
-
-        public int QuantityOnHand { get; set; }
-
-        public int ReorderLevel { get; set; }
-
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        public DateTime? UpdatedAt { get; set; }
-
-        public ICollection<StockBatch> StockBatches { get; set; } = new List<StockBatch>();
+        get => QuantityOnHand;
+        set => QuantityOnHand = value;
     }
+
+    public int ReorderLevel { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<StockBatch> StockBatches { get; set; } = new List<StockBatch>();
 }
